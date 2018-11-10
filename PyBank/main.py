96455c732206071d7a10d38ffa5edcd7463f9c72
Gabe Thesing <gthesing74@gmail.com>
2018-11-09 21:05:48 -0600

    updated pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,10 @@ g_profit = 0            # amount for greatest increase in profit
 g_loss = 0              # amount for greatest decrease in profit
 g_profit_date = ''      # month of greatest profit increase
 g_loss_date = ''        # month of greatest profit decrease
+current = 0
+previous = 0
+change = 0
+delta = 0
 
 with open(path, newline = '') as BudgetData:
     csvreader = csv.reader(BudgetData, delimiter=',')
@@ -20,31 +24,39 @@ with open(path, newline = '') as BudgetData:
         else:
             item = row[1]
             date = item[0]
-            pro = float(item[1])
+            current = float(item[1])
             # print(f'Profit/Loss was {pro} for {date}')    # Useful for editing
             months = months + 1
-            profit = profit + (pro)
+            profit = profit + current
+            delta = current - previous
 
-            if pro > g_profit: 
-                g_profit = pro
+            if row[0] == 1:         #pls dont ask me to explain this
+                change = 0          #was like 5 brewskis deep when I wrote it
+            else:
+                change = change + (current - previous)
+
+            if delta > g_profit:
+                g_profit = delta
                 g_profit_date = date
-            if pro < g_loss:
-                g_loss = pro
+            if delta < g_loss:
+                g_loss = delta
                 g_loss_date = date
 
-average_change = round(profit/months,2)
+        previous = current
+
+average_change = round(change/(months-1), 2)
 
 print(f'\nFinancial Analysis \n---------------------------------------------------------')
 print(f'Total Months: {months}')
 print(f'Total: ${profit}')
 print(f'Average Change: ${average_change}')
-print(f'Greatest Increase in Profits: {g_profit_date} ({g_profit})')
-print(f'Greatest Decrease in Profits: {g_loss_date} ({g_loss})')
+print(f'Greatest Increase in Profits: {g_profit_date} (${g_profit})')
+print(f'Greatest Decrease in Profits: {g_loss_date} (${g_loss})')
 print('---------------------------------------------------------')
 
 
 
-""" # froze the terminal so probably outdated...
+""" 
 with open("PyBank.txt", "w") as output:
     subprocess.call(["python", "/main.py" ], stdout=output)
 """
